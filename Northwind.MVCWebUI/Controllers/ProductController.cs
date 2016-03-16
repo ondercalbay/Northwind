273e5e64d16aaf124c9fba8036e4f7ee152e3ec2
Önder ÇALBAY <ondercalbay@hotmail.com>
2016-03-17 00:20:44 +0200

    MVC

---
+++ Northwind.MVCWebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Northwind.Bll.Concrate;
+using Northwind.Dal.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Northwind.MVCWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ProductManager _productManager = new ProductManager(new EfProductDal());
+
+        public ViewResult Index()
+        {
+            return View(_productManager.GetAll());
+        }
+         
+    }
+}
