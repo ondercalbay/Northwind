bb8b0dd0f6ae542786b9a5e6b2504c51d19e29d7
Önder ÇALBAY <ondercalbay@hotmail.com>
2016-03-30 22:58:20 +0300

    Cart

---
+++ Northwind.Entities/Cart.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Northwind.Entities
+{
+    public class Cart
+    {
+        List<CartLine> _lines = new List<CartLine>();
+
+        public void AddToCart(Product product, int quantity)
+        {
+            CartLine cartLine = _lines.FirstOrDefault(c => c.Product.ProductId == product.ProductId);
+
+            if (cartLine == null)
+            {
+                _lines.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                cartLine.Quantity += quantity;
+            }
+        }
+        public void RemoveFromCart(Product product)
+        {
+            _lines.RemoveAll(p => p.Product.ProductId == product.ProductId);
+        }
+
+        public decimal Total
+        {
+            get { return _lines.Sum(c => c.Product.UnitPrice * c.Quantity); }
+        }
+
+        public void Clear()
+        {
+            _lines.Clear();
+        }
+
+        public List<CartLine> Lines
+        {
+            get { return _lines; }
+        }
+
+    }
+
+    
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get { return this.Product.UnitPrice * this.Quantity; } }
+    }
+}
--- Northwind.MVCWebUI/Controllers/CartController.cs
+++ Northwind.MVCWebUI/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Northwind.Entities;
+using Northwind.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,53 @@ namespace Northwind.MVCWebUI.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
+        IProductService _productService;
+
+
+        public  CartController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ActionResult Index()
+        {
+            var cart = (Cart)Session["cart"];
+            return View("Index", cart);
+        }
+
         public ActionResult AddToCart(int productId)
         {
-            
+            Product product = _productService.Get(productId);
+            Cart cart;
+            if (Session["cart"]==null)
+            {
+                cart = new Cart();
+                Session["cart"] = cart;
+            }
+            else
+            {
+                cart = (Cart)Session["cart"];
+            }
+             
+            cart.AddToCart(product, 1);
+
+            return RedirectToAction("Index",cart);
+        }
+
+        public ActionResult RemoveFromCart(int productId)
+        {
+            var cart = (Cart)Session["cart"];
+            if (cart.Lines.Count == 0)
+            {
+                ModelState.AddModelError("", "Sepette bu ürün yok");
+            }
+            else
+            {
+                Product product = _productService.Get(productId);
+                cart.RemoveFromCart(product);
+            }
+
+            return RedirectToAction("Index", cart);
         }
     }
 }
