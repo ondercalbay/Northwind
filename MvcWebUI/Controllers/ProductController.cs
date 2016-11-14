4612013d0060608a6a12ce31829a926de94644c0
ondercalbay <ondercalbay@hotmail.com>
2016-11-14 18:03:09 +0300

    UI

---
+++ MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Northwind.Bll;
+using Northwind.Dal.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager _productManager = new ProductManager(new EfProductDal());
+        
+        public ViewResult Index()
+        {
+            return View(_productManager.GetAll());
+        }
+    }
+}
