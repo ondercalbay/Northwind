6d980a0ac562ea1d92babd9c3db1ff93304c5319
ondercalbay <ondercalbay@hotmail.com>
2016-11-14 17:28:01 +0300

    WcfLibraru

--- Northwind.Interfaces/IProductService.cs
+++ Northwind.Interfaces/IProductService.cs
@@ -2,17 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Northwind.Interfaces
 {
+    [ServiceContract]
     public interface IProductService
     {
+        [OperationContract]
         List<Product> GetAll();
+
+        [OperationContract]
         Product Get(int productId);
+
+        [OperationContract]
         void Add(Product product);
+
+        [OperationContract]
         void Update(Product product);
+
+        [OperationContract]
         void Delete(int productId);
     }
 }
---
+++ Northwind.WcfLibrary/Concrete/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using Northwind.Bll;
+using Northwind.Dal.Concrete.EntityFramework;
+using Northwind.Entities;
+using Northwind.Interfaces;
+using System.Collections.Generic;
+
+namespace Northwind.WcfLibrary.Concrete
+{
+    public class ProductService : IProductService
+    {
+        //Instance Provider ile ezilecek
+        private ProductManager _productManager = new ProductManager(new EfProductDal());
+        
+        public void Add(Product product)
+        {
+            _productManager.Add(product);
+        }
+
+        public void Delete(int productId)
+        {
+            _productManager.Delete(productId);
+        }
+
+        public Product Get(int productId)
+        {
+            return _productManager.Get(productId);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productManager.GetAll();
+        }
+
+        public void Update(Product product)
+        {
+            _productManager.Update(product);
+        }
+    }
+}
