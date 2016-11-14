040ed629abf267757a0f2ea4f863dffdb3182a9b
ondercalbay <ondercalbay@hotmail.com>
2016-11-14 17:16:47 +0300

    Bll

---
+++ Northwind.Bll/ProductManager.cs
@@ -0,0 +1,47 @@
+﻿using Northwind.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Entities;
+using Northwind.Dal.Concrete.EntityFramework;
+using Northwind.Dal.Abstract;
+
+namespace Northwind.Bll
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(int productId)
+        {
+            _productDal.Delete(productId);
+        }
+
+        public Product Get(int productId)
+        {
+            return _productDal.Get(productId);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
